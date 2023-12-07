3c11be7e0e219be958e5fa18c630dd6b9bf47c31
MattiaSalve <mattiasalvetti@Mattias-MacBook-Air.local>
2023-12-07 17:43:00 +0100

    created first class

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Physics-engine
+
+A simple physics engine in 2D for didactic means.
---
+++ src/objects.cpp
@@ -0,0 +1,8 @@
+class myObject{
+  public:
+    float position[2];
+    float velocity[2];
+    float force[2];
+    float mass;
+    float acceleration[2];
+};
